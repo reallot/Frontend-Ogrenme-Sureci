61445ed0bc9a0a96b300c353a1cd91ca00274622
Emirhan Ayaz <84610899+reallot@users.noreply.github.com>
2025-07-14 23:57:05 +0300

    Gün - 5 ve Proje - 2 ekleme

---
+++ Gün-5 ve Proje-2/Gün-5/app.js
@@ -0,0 +1,134 @@
+// console.log("selam");
+// document.writeln("<br><br>Selamlar");
+// console.log(window);
+
+// for(var i = 0; i < 5; i++){
+//     document.writeln("<br><br> Selam" + i)
+// }
+
+
+/* var ve let/const farkı
+
+var function scope;
+let/const block scope;
+
+*/
+
+
+// let a = 12;
+// let b = 26;
+// alert("A + B " + (a+b) + "'e eşittir");
+
+
+/*
+? Veri Tipleri
+
+1-String
+2-Number
+3-Boolean
+4-Null
+5-Undefined -- tanımsız
+6-Object
+7-Function
+
+typeof(variable)  ile  variable'ın veri tipi
+görülebilir.
+
+== ile sadece içerik,
+=== ile hem içerik hem veri tipi kontrol edilir
+örnek : == 5 ile "5"' i aynı olarak görür o yüzden true döner
+ancak === false döner.
+x**y = x^y.
+*/
+
+
+// function varTest(){
+//     var selam = "Selam";
+//     if(true){
+//         var varTest01 = 27;
+//         const constTest0 = 37;
+//         let letTest0 = 47;
+//         console.log(constTest0);
+//         console.log(letTest0);
+//     }
+    
+//     console.log(varTest01);
+//     console.log(constTest0); //Çalışmadı
+//     console.log(letTest0); // Çalışmadı
+//     console.log(selam);
+// }
+
+// varTest();
+
+// javadaki scanner = prompt
+
+// prompt("Adınızı\n ve soyadınızı giriniz.")
+
+// debugger;
+var isim = prompt("Adınızı giriniz");
+console.log(isim);
+
+var tercih = confirm("Emin misiniz ?")
+
+console.log(typeof tercih);
+// Confirm evet hayır kutucuğu çıkartır
+// tamama basılırsa true iptale basılırsa false döner.
+if(tercih === true){
+    console.log("Onay verdiniz.")
+}else{
+    console.log("Ret verdiniz.")
+}
+
+// javadaki typecasting gibi Number(string) denebilir.
+// parseInt() de kullanılabilir.
+// parseFloat() floatlar için.
+// toString() veya String() ile stringe çevrilebilir.
+// arrayin içindekiler string,numbera çevrilebilir aynı yöntemlerle.
+// Math.floor() sayıyı yuvarlar
+// array için || var arrayName = [deger1,deger2]
+
+// ! Dizinin Metotları
+/*
+?push: dizinin sonuna eleman ekler, ayrica dizinin uzunlugunu döner
+?unshift : dizinin basina eleman ekler, eleman sayisini geriye döner
+
+?pop: dizinin sonundan eleman siler , eleman sayisini döner
+?shift : dizinin basindan eleman sile , eleman sayisini döner
+
+?splice(index, incdex) :eleman eklemek ve silmek için kullanilir.
+
+?toString: diziyi stringe cevirebiliriz.
+?join: diziyi stringe cevirir Fark ise araya eleman ekleyebiliriz
+
+?concat: dizileri birlestirmek için kullanilir.
+?slice(dilimlemek): diziyi istenilen yerden bölüp yeni bir dizi olustu
+?length : dizinin uzunlugunu verir.
+?reverse : dizinin elemanlarin ters cevirmek
+?split(bölmek) : belirli bir ifadeye göre bölüp diziye cevirmek.
+?indexOf : elemanin index numarasini verir.
+?includes: verilen eleman içeriyor mu ona bakar
+
+*/
+
+let num = prompt("1-3 arasında bir sayı seçiniz.");
+
+switch(num){
+    case "1" :
+        console.log("1'i seçtiniz")
+        break;
+    case "2" :
+        console.log("2'yi seçtiniz")
+        break;
+    case "3" :
+        console.log("3'ü seçtiniz")
+        break;           
+}
+
+var sayi = 21;
+stringg = sayi.toString();
+
+console.log(typeof stringg);
+
+var num0 = 27;
+var num1 = num0/2;
+document.write(num1);
